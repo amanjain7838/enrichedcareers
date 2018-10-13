a8c3eaed3fa8d574dcfb1f085758a194a6165179
Aman Jain <amanjain7838@gmail.com>
2018-10-13 22:22:37 +0530

    13oct

---
+++ src/AppBundle/Controller/detailController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class detailController extends Controller
+{
+    public function jobDetailAction(${id)
+    {
+        return $this->render('AppBundle:detail:job_detail.html.twig', array(
+            // ...
+        ));
+    }
+
+}
