10d7377bbacbbf69cc4a0eba982b8ee7ffc8cfbc
Aman Jain <amanjain7838@gmail.com>
2018-10-13 22:42:39 +0530

    13oct

--- src/AppBundle/Controller/detailController.php
+++ src/AppBundle/Controller/detailController.php
@@ -6,9 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class detailController extends Controller
 {
-    public function jobDetailAction(${id)
+    public function jobDetailAction($id)
     {
-        return $this->render('AppBundle:detail:job_detail.html.twig', array(
+        return $this->render('detail/job_detail.html.twig', array(
             // ...
         ));
     }
